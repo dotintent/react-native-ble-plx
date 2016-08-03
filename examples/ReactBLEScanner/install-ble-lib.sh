d5a24c5ecd12357d955a13560de02c50e70bf6bd
Przemysław Lenart <przemek.lenart@polidea.com>
2016-08-03 12:59:28 +0200

    Update sync and install scripts

--- examples/ReactBLEScanner/install-ble-lib.sh
+++ examples/ReactBLEScanner/install-ble-lib.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-rsync -a --delete --progress ../../ ./node_modules/react-native-ble-plx --exclude examples --exclude '.*' --exclude ios/BleClientManager/Carthage
+rsync -a --delete --progress ../../ ./node_modules/react-native-ble-plx \
+ --exclude examples \
+ --exclude '.*' \
+ --exclude ios/BleClientManager/Carthage \
+ --exclude android/build \
+ --exclude android/react-native-ble-plx.iml \
+ --exclude README.md
 
--- examples/ReactBLEScanner/sync-ble-lib.sh
+++ examples/ReactBLEScanner/sync-ble-lib.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-rsync -a --delete --progress ./node_modules/react-native-ble-plx/ ../../ --exclude examples --exclude '.*' --exclude ios/BleClientManager/Carthage
+rsync -a --delete --progress ./node_modules/react-native-ble-plx/ ../../ \
+ --exclude examples \
+ --exclude '.*' \
+ --exclude ios/BleClientManager/Carthage \
+ --exclude android/build \
+ --exclude android/react-native-ble-plx.iml \
+ --exclude README.md
