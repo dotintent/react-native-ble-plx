76a0216d4f8d4b0beebb67917f7ac12e25e0e207
Przemysław Lenart <przemek.lenart@polidea.com>
2016-10-27 11:10:18 +0200

    Properly handle conversion from String -> CBUUID. Invalid UUIDs resulted in exceptions.

--- ios/BleClientManager/BleUtils.swift
+++ ios/BleClientManager/BleUtils.swift
@@ -24,8 +24,19 @@ extension SequenceType where Generator.Element == String {
 
 extension String {
     func toCBUUID() -> CBUUID? {
-        let value = self.characters.count == 4 ? "0000\(self)-0000-1000-8000-00805f9b34fb" : self
-        return CBUUID(string: value)
+        let uuid: String
+        switch self.characters.count {
+        case 4:
+            uuid = "0000\(self)-0000-1000-8000-00805f9b34fb"
+        case 8:
+            uuid = "\(self)-0000-1000-8000-00805f9b34fb"
+        default:
+            uuid = self
+        }
+        guard let nsuuid = NSUUID(UUIDString: uuid) else {
+            return nil
+        }
+        return CBUUID(NSUUID: nsuuid)
     }
 }
 
