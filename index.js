f96e7f46ef2c184244a1b14828432c8ace9b9dac
Przemysław Lenart <przemek.lenart@polidea.com>
2017-05-25 16:19:27 +0200

    Reexport LogLevel constant.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ export { Device } from './src/Device'
 export { Service } from './src/Service'
 export { Characteristic } from './src/Characteristic'
 export { fullUUID } from './src/Utils'
-export { State } from './src/TypeDefinition'
+export { State, LogLevel } from './src/TypeDefinition'
 
 export type {
   Subscription,
