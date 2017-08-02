780e75732f511885df33e61738da0714291d9441
Radek Czemerys <radko93@gmail.com>
2017-08-02 18:45:08 +0200

    Remove createJSModules

--- android/src/main/java/com/polidea/reactnativeble/BlePackage.java
+++ android/src/main/java/com/polidea/reactnativeble/BlePackage.java
@@ -12,11 +12,6 @@ import java.util.List;
 public class BlePackage implements ReactPackage {
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
