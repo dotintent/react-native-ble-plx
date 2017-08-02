02f3d27b20841123c801a522eafdf3dfdf2c3233
Radek Czemerys <radko93@gmail.com>
2017-08-02 19:07:45 +0200

    Remove only @Override marker

--- android/src/main/java/com/polidea/reactnativeble/BlePackage.java
+++ android/src/main/java/com/polidea/reactnativeble/BlePackage.java
@@ -10,6 +10,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class BlePackage implements ReactPackage {
+    
+    public List<Class<? extends JavaScriptModule>> createJSModules() {		
+        return Collections.emptyList();		
+    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
