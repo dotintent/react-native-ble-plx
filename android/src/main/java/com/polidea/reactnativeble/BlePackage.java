eeaf883e555ca445238fad8f739eac177c181dc5
Radek Czemerys <radko93@gmail.com>
2017-08-02 19:52:48 +0200

    Added comment

--- android/src/main/java/com/polidea/reactnativeble/BlePackage.java
+++ android/src/main/java/com/polidea/reactnativeble/BlePackage.java
@@ -10,9 +10,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class BlePackage implements ReactPackage {
-    
-    public List<Class<? extends JavaScriptModule>> createJSModules() {		
-        return Collections.emptyList();		
+
+    // Deprecated RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
     }
 
     @Override
