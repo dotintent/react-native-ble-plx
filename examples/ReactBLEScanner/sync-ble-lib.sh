8ad5bd686ebd92e520c11a3430c4c0c436990d54
Jerzy Kleszcz <jerzy.kleszcz@polidea.com>
2017-12-01 10:54:23 +0100

    Replaced old example app with new project with reused app identifiers and certificates, initial dependencies configuration.

--- examples/ReactBLEScanner/sync-ble-lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rsync -a --delete --progress ./node_modules/react-native-ble-plx/ ../../ \
- --exclude examples \
- --exclude '.*' \
- --exclude ios/BleClientManager/Carthage \
- --exclude android/build \
- --exclude android/react-native-ble-plx.iml \
- --exclude README.md
