f9170005826028fdbf944e667015b153e1ec517d
Adrian Carolli <adrian.carolli1@gmail.com>
2017-12-04 15:57:04 -0500

    remove verbose and fix typo

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 // jest.config.js
 module.exports = {
-  verbose: true,
   preset: "react-native",
   // Only include files directly in __tests__, not in nested folders.
   testRegex: '/__tests__/[^/]*(\\.js|\\.coffee|[^d]\\.ts)$',
-  "modulePathIgnorePatterns": ["<rootDir>/examples/ReactBLEScanner/noode_modules/*"]
+  "modulePathIgnorePatterns": ["<rootDir>/examples/ReactBLEScanner/node_modules/*"]
 }
