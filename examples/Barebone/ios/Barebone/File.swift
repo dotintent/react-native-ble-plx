0ad8b63e63dfe5185a3cc6913e7ebd2458ab9569
Piotr Dubiel <piotr.dubiel@polidea.com>
2019-02-01 18:58:33 +0100

    Barebone example

---
+++ examples/Barebone/ios/Barebone/File.swift
@@ -0,0 +1,5 @@
+//
+//  Emmpty file for react-native-ble-plx integration purposes.
+//  For further info head to: https://github.com/Polidea/react-native-ble-plx#configuration--installation
+
+import Foundation
