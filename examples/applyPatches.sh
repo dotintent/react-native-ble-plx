2139e3fe19ca91e26d510f5017f3353f702a87bd
Piotr Dubiel <piotr.dubiel@polidea.com>
2019-02-08 19:11:20 +0100

    New patches

--- examples/applyPatches.sh
+++ examples/applyPatches.sh
@@ -2,7 +2,7 @@
 
 REACT_NATIVE_VERSION=$1
 
-for filename in ./patches/rn-$1/*.patch; do
+for filename in ../patches/rn-$1/*.patch; do
   echo Applying "$filename"
   git apply "$filename" || echo Patch not applied.
 done
