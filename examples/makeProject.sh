91b9827741e7cec488af6cda07e7dcb8f66f1bc9
Piotr Dubiel <piotr.dubiel@polidea.com>
2019-02-08 22:13:43 +0100

    Restore old patches

--- examples/makeProject.sh
+++ examples/makeProject.sh
@@ -8,9 +8,6 @@ react-native init --version="$RN_VERSION" Setup
 cd Setup
 npm install ../.. --save
 react-native link react-native-ble-plx
-git init
-git add .
-git commit -m 'init'
 bash ../applyPatches.sh $RN_VERSION
 cd ..
 ruby enableSwift.rb
