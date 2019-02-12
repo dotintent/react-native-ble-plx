d243f7693531f1fffdac725b9f0023d9faec22fb
Piotr Dubiel <piotr.dubiel@polidea.com>
2019-02-12 14:58:55 +0100

    Use yarn for generating setup project

--- examples/installFlow.sh
+++ examples/installFlow.sh
@@ -2,4 +2,4 @@
 
 FLOW_VERSION=$(cat .flowconfig| grep -A 1 '\[version\]' | tail -1)
 
-npm install flow-bin@$FLOW_VERSION
+yarn add flow-bin@$FLOW_VERSION
--- examples/makeProject.sh
+++ examples/makeProject.sh
@@ -6,7 +6,7 @@ RN_VERSION=$1
 
 react-native init --version="$RN_VERSION" Setup
 cd Setup
-npm install ../.. --save
+yarn add ../..
 react-native link react-native-ble-plx
 bash ../applyPatches.sh $RN_VERSION
 cd ..
