c71d1c2a7745c29a3719fbc437124b22583151d6
Piotr Dubiel <piotr.dubiel@polidea.com>
2019-02-15 14:34:26 +0100

    Run flow in root project

--- integration-tests/on-device-diagnose/src/utils.js
+++
@@ -1,7 +0,0 @@
-import { Platform } from 'react-native';
-
-export const addAccessibilityLabel = (id: string) => Platform.select({
-  ios: { testID: id },
-  android: { accessible: true, accessibilityLabel: id }
-});
-
