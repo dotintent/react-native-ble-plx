032ebfb1d24c3240dc4453ae35d132a608c7e0e2
Piotr Dubiel <piotr.dubiel@polidea.com>
2019-02-15 18:57:28 +0100

    Update emulator script

--- integration-tests/scripts/runAndroidEmulator.sh
+++ integration-tests/scripts/runAndroidEmulator.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a -c 100M
-emulator -avd test -no-audio -no-window &
+echo no | avdmanager create avd --force -n test -k "system-images;android-22;default;armeabi-v7a"
+$ANDROID_HOME/emulator/emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
+adb shell settings put global window_animation_scale 0 &
+adb shell settings put global transition_animation_scale 0 &
+adb shell settings put global animator_duration_scale 0 &
 adb shell input keyevent 82 &
 
 echo "Emulator is ready"
