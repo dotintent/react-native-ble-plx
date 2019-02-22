a8d8f2979faa6a95c3b4beb873acb9b9ff4b2287
Piotr Dubiel <piotr.dubiel@polidea.com>
2019-02-22 12:18:10 +0100

    More logs for emulator script

--- integration-tests/scripts/runAndroidEmulator.sh
+++ integration-tests/scripts/runAndroidEmulator.sh
@@ -1,12 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash
 
-echo no | android create avd --force -n test -t android-22 --abi google_apis/armeabi-v7a
-emulator -avd test -no-audio -no-window &
+EMULATOR_NAME=test
+
+echo "Creating emulator..."
+echo no | android create avd --force -n $EMULATOR_NAME -t android-22 --abi google_apis/armeabi-v7a
+echo "Booting emulator - $EMULATOR_NAME..."
+emulator -avd $EMULATOR_NAME -no-audio -no-window &
+echo "Waiting for emulator..."
 android-wait-for-emulator
-android-wait-for-emulator
+echo "Disabling animations..."
 adb shell settings put global window_animation_scale 0 &
 adb shell settings put global transition_animation_scale 0 &
 adb shell settings put global animator_duration_scale 0 &
+echo "Unlocking emulator..."
 adb shell input keyevent 82 &
 
 echo "Emulator is ready"
