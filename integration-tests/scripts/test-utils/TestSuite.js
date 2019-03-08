f386f5af54352800fdf8f102521b6969a479a2c4
Piotr Dubiel <piotr.dubiel@polidea.com>
2019-03-08 13:55:11 +0100

    Setup Earl Grey

--- integration-tests/scripts/test-utils/TestSuite.js
+++ integration-tests/scripts/test-utils/TestSuite.js
@@ -1,9 +1,16 @@
 // @flow
 
 import React from 'react'
-import { View } from 'react-native'
+import { SafeAreaView,StyleSheet } from 'react-native'
 import { addAccessibilityLabel } from './utils'
 import TestCase, { type TestCaseType } from './TestCase'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'papayawhip'
+  }
+})
 
 type Props = {
   testCases: Array<TestCaseType>
@@ -13,11 +20,11 @@ export default class TestSuite extends React.Component<Props> {
   render() {
     const { testCases } = this.props
     return (
-      <View {...addAccessibilityLabel('TestSuite')}>
+      <SafeAreaView style={styles.container} {...addAccessibilityLabel('TestSuite')}>
         {testCases.map(({ name, run }) => (
           <TestCase key={`testCase-${name}`} name={name} run={run} />
         ))}
-      </View>
+      </SafeAreaView>
     )
   }
 }
