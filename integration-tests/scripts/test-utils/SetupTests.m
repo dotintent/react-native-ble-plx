acf5b64df1def107749edf82aed924daf7186ffc
Piotr Dubiel <piotr.dubiel@polidea.com>
2019-03-13 11:03:43 +0100

    Additional wait in tests

--- integration-tests/scripts/test-utils/SetupTests.m
+++ integration-tests/scripts/test-utils/SetupTests.m
@@ -7,6 +7,19 @@
 @implementation SetupTests
 
 - (void)testBleManager {
+  GREYCondition *waitForStatus = [GREYCondition conditionWithName:@"wait for loading finish" block:^BOOL{
+    NSError *error;
+        [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"TestCaseStatus")]
+         assertWithMatcher:grey_anyOf(
+                                      grey_accessibilityLabel(@"success"),
+                                      grey_accessibilityLabel(@"failure"),
+                                      nil
+                                      ) error:&error];
+        return error == nil;
+  }];
+  
+  [waitForStatus waitWithTimeout:60];
+
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"TestCaseStatus")]
     assertWithMatcher:grey_accessibilityLabel(@"success")];
 }
