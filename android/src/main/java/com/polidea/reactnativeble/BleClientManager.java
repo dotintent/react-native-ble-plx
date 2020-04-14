00553bf4d424bc72799dc258a32c889f91d79eb0
Przemysław Lenart <przemek.lenart@polidea.com>
2020-04-14 11:55:14 +0200

    Migrate import statements to AndroidX

--- android/src/main/java/com/polidea/reactnativeble/BleClientManager.java
+++ android/src/main/java/com/polidea/reactnativeble/BleClientManager.java
@@ -1,7 +1,7 @@
 package com.polidea.reactnativeble;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
