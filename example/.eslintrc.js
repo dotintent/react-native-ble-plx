5c68fde8f5b54de382052a66dd6ef6264580404d
mciechanowicz <mciechanowicz94@gmail.com>
2021-12-23 13:22:46 +0000

    allowTemplateLiterals rule

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -3,5 +3,7 @@ module.exports = {
   extends: '@react-native-community',
   rules: {
     semi: [1, 'never'],
+    'react-native/no-inline-styles': 0,
+    quotes: [1, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
   },
 }
