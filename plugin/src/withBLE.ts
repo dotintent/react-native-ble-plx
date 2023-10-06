05cea28aa5aa8e991bb5a85603d7196051d65ceb
Kacper Cyranowski <kacper.cyranowski@gmail.com>
2023-10-06 08:27:35 +0200

    feat: add initial plugin

---
+++ plugin/src/withBLE.ts
@@ -0,0 +1,51 @@
+import { AndroidConfig, ConfigPlugin, createRunOncePlugin, WarningAggregator } from '@expo/config-plugins'
+
+import { withBLEAndroidManifest } from './withBLEAndroidManifest'
+import { BackgroundMode, withBLEBackgroundModes } from './withBLEBackgroundModes'
+import { withBluetoothPermissions } from './withBluetoothPermissions'
+
+const pkg = { name: 'react-native-ble-plx', version: 'UNVERSIONED' } // require('react-native-ble-plx/package.json')
+
+/**
+ * Apply BLE native configuration.
+ */
+const withBLE: ConfigPlugin<
+  {
+    isBackgroundEnabled?: boolean
+    neverForLocation?: boolean
+    modes?: BackgroundMode[]
+    bluetoothAlwaysPermission?: string | false
+  } | void
+> = (config, props = {}) => {
+  const _props = props || {}
+  const isBackgroundEnabled = _props.isBackgroundEnabled ?? false
+  const neverForLocation = _props.neverForLocation ?? false
+
+  if ('bluetoothPeripheralPermission' in _props) {
+    WarningAggregator.addWarningIOS(
+      'bluetoothPeripheralPermission',
+      `The iOS permission \`NSBluetoothPeripheralUsageDescription\` is fully deprecated as of iOS 13 (lowest iOS version in Expo SDK 47+). Remove the \`bluetoothPeripheralPermission\` property from the \`@config-plugins/react-native-ble-plx\` config plugin.`
+    )
+  }
+
+  // iOS
+  config = withBluetoothPermissions(config, _props)
+  config = withBLEBackgroundModes(config, _props.modes || [])
+
+  // Android
+  config = AndroidConfig.Permissions.withPermissions(config, [
+    'android.permission.BLUETOOTH',
+    'android.permission.BLUETOOTH_ADMIN',
+    'android.permission.BLUETOOTH_CONNECT' // since Android SDK 31
+  ])
+  config = withBLEAndroidManifest(config, {
+    isBackgroundEnabled,
+    neverForLocation
+  })
+
+  return config
+}
+
+export { BackgroundMode }
+
+export default createRunOncePlugin(withBLE, pkg.name, pkg.version)
